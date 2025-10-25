a51362f02afe3d4624ec454edbfb99e0bc87df79
malakhovs04 <malakhovs04@mail.ru>
2025-10-25 15:31:50 +0300

    Solved

--- Reorder_Linked_List.py
+++ Reorder_Linked_List.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -38,47 +37,3 @@ class Solution:
             
             first = temp1
             second = temp2
-
-=======
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head or not head.next:
-            return
-        
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        
-        second_half = slow.next
-        slow.next = None  
-        prev = None
-        current = second_half
-        
-        while current:
-            next_temp = current.next
-            current.next = prev
-            prev = current
-            current = next_temp
-        
-        first = head
-        second = prev
-        
-        while second:
-            temp1 = first.next
-            temp2 = second.next
-            
-            first.next = second
-            second.next = temp1
-            
-            first = temp1
-            second = temp2
-
->>>>>>> 9dda5c45882b8ba8978b316d89ef05360b56d51d
-    
