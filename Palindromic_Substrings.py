e982095c519cbd4afed946dcabed6e61e19b8f19
malakhovs04 <malakhovs04@mail.ru>
2025-11-22 18:46:12 +0300

    Solved

---
+++ Palindromic_Substrings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        def man(s):
+            t = "#" + "#".join(s) + "#"
+            n = len(t)
+            p = [0] * n
+            l, r = 0, 0
+            for i in range(n):
+                p[i] = min(r-1, p[l + (r-i)]) if i < r else 0
+                while(i+p[i]+1 < n and i-p[i]-1>=0 and t[i+p[i]+1]==t[i-p[i]-1]):
+                    p[i]+=1
+                if i + p[i] >r:
+                    l,r = i-p[i], i+p[i]
+            return p
+
+        p = man(s)
+        res = 0
+        for i in p:
+           res +=(i+1)//2
+        return res
